fdf83477f62198c5ca8cbc5ede64790bb9b90679
Yevgeniy Kiveisha <yevgeniy.kiveisha@hp.com>
2020-03-04 10:20:00 +0200

    First commit

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.d
@@ -0,0 +1,80 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/workspace/Projects/stm32f407_bootloader/Inc/stm32f4xx_hal_conf.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/core_cm4.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Inc/stm32f4xx_hal_conf.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/workspace/Projects/stm32f407_bootloader/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
